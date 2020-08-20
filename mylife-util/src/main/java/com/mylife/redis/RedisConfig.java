43cb4c6ee53d91165b9a1aa5cb643212904d0710
wangifxue <wangyihe@wanzhexue.com>
2020-08-21 00:22:27 +0800

    菜逼(wyh)，何时才去整体的学习下maven?

--- mylife-util/src/main/java/com/mylife/redis/RedisConfig.java
+++ mylife-util/src/main/java/com/mylife/redis/RedisConfig.java
@@ -7,62 +7,27 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.*;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
-
-import java.time.Duration;
 
 /**
  * @descirption : redis配置类
  * @author : wyh
  * @date : 2020/6/28 11:10
  */
-//@Configuration
-//@EnableCaching
+@Configuration
+@EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
 
-    @Bean
-    public RedisConnectionFactory connectionFactory() {
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-//        poolConfig.setMaxTotal(maxActive);
-//        poolConfig.setMaxIdle(maxIdle);
-//        poolConfig.setMaxWaitMillis(maxWait);
-//        poolConfig.setMinIdle(minIdle);
-//        poolConfig.setTestOnBorrow(true);
-//        poolConfig.setTestOnReturn(false);
-//        poolConfig.setTestWhileIdle(true);
-        JedisClientConfiguration clientConfig = JedisClientConfiguration.builder()
-                .usePooling().poolConfig(poolConfig).and().readTimeout(Duration.ofMillis(1000)).build();
-
-        // 单点redis
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-        // 哨兵redis
-        // RedisSentinelConfiguration redisConfig = new RedisSentinelConfiguration();
-        // 集群redis
-        // RedisClusterConfiguration redisConfig = new RedisClusterConfiguration();
-        redisConfig.setHostName("localhost");
-        redisConfig.setPassword(RedisPassword.of(""));
-        redisConfig.setPort(6379);
-        redisConfig.setDatabase(0);
-
-        return new JedisConnectionFactory(redisConfig,clientConfig);
-    }
-
 
     @Bean
-//    @SuppressWarnings("all")
     public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory factory) {
-        System.out.println("-----注册redisTemplate------");
         RedisTemplate<String,Object> template = new RedisTemplate<>();
         // 配置连接工厂
         template.setConnectionFactory(factory);
@@ -89,13 +54,13 @@ public class RedisConfig extends CachingConfigurerSupport {
         return template;
     }
 
-//    @Bean
-//    public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate) {
-//        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
-//        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
-//        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
-//    }
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate) {
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
+    }
 
     /**
      * 对hash类型的数据操作
