f0d94ab9c47bd1c44d2c16fddbf93e64ca712a0b
wangifxue <1178879424@qq.com>
2020-08-21 18:49:49 +0800

    MyBaits-Plus 乐观锁插件

--- mylife-mapper/src/main/java/com/mylife/config/MybatisPlusConfig.java
+++ mylife-mapper/src/main/java/com/mylife/config/MybatisPlusConfig.java
@@ -1,5 +1,6 @@
 package com.mylife.config;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import com.mylife.config.mysqlInjector.base.MySqlInjector;
@@ -34,5 +35,14 @@ public class MybatisPlusConfig {
         // 开启 count 的 join 优化,只针对部分 left join
         paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
         return paginationInterceptor;
+    }
+
+    /**
+     * 乐观锁插件
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
     }
 }
