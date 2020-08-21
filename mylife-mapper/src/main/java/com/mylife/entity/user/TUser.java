baada8eb623fded1687474f26d8187965bc64e31
wangifxue <1178879424@qq.com>
2020-08-21 14:52:11 +0800

    pom优化

--- mylife-mapper/src/main/java/com/mylife/entity/user/TUser.java
+++ mylife-mapper/src/main/java/com/mylife/entity/user/TUser.java
@@ -3,6 +3,8 @@ package com.mylife.entity.user;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
@@ -13,6 +15,7 @@ import java.io.Serializable;
  * @author wyh
  * @since 2020-08-20
  */
+@Data
 public class TUser extends Model<TUser> {
 
     private static final long serialVersionUID = 2899824301520106139L;
@@ -20,55 +23,23 @@ public class TUser extends Model<TUser> {
     /**
      * 主键
      */
-        @TableId(value = "id", type = IdType.AUTO)
-      private Long id;
-
-      /**
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+    
+    /**
      * 用户名
      */
-      private String userName;
+    private String userName;
 
-      /**
+    /**
      * 密码
      */
-      private String password;
+    private String password;
 
-    
-    public Long getId() {
-        return id;
-    }
-
-      public void setId(Long id) {
-          this.id = id;
-      }
-    
-    public String getUserName() {
-        return userName;
-    }
-
-      public void setUserName(String userName) {
-          this.userName = userName;
-      }
-    
-    public String getPassword() {
-        return password;
-    }
-
-      public void setPassword(String password) {
-          this.password = password;
-      }
 
     @Override
     protected Serializable pkVal() {
-          return this.id;
-      }
-
-    @Override
-    public String toString() {
-        return "TUser{" +
-              "id=" + id +
-                  ", userName=" + userName +
-                  ", password=" + password +
-              "}";
+        return this.id;
     }
+
 }
